74aa20d6de1916fed3c3dddaeec04d9403e0a146
mumbamwale-jpg <mumbamwale@gmail.com>
2026-02-22 03:07:44 +0200

    add server logic

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
+import express from "express";
+import { sendMessage } from "./ultramsg.js";
+import { handleFlow } from "./flow.js";
 
+const app = express();
+app.use(express.json());
+
+let sessions = {};
+
+app.post("/webhook", async (req, res) => {
+  const { from, body } = req.body;
+
+  if (!sessions[from]) {
+    sessions[from] = {};
+  }
+
+  const result = handleFlow(sessions[from], body);
+
+  sessions[from] = result.state;
+
+  await sendMessage(from, result.reply);
+
+  res.sendStatus(200);
+});
+
+app.get("/", (req, res) => {
+  res.send("Admissions assistant is running.");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("Server running on port", port));
