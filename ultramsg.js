b9a43cca189d0c2ff4e7c1b75cbb842ca19b559f
mumbamwale-jpg <mumbamwale@gmail.com>
2026-02-22 03:06:09 +0200

    add ultramsg.js

--- ultramsg.js
+++ ultramsg.js
@@ -1,1 +1,18 @@
+import axios from "axios";
 
+const instanceId = process.env.INSTANCE_ID;
+const token = process.env.ULTRAMSG_TOKEN;
+
+export async function sendMessage(to, body) {
+  const url = `https://api.ultramsg.com/${instanceId}/messages/chat`;
+
+  try {
+    await axios.post(url, {
+      token,
+      to,
+      body
+    });
+  } catch (error) {
+    console.error("Error sending message:", error.response?.data || error.message);
+  }
+}
